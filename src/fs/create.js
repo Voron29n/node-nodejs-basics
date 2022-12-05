c44f998ac061a608936c1784f30b845c8a4b73a7
Pavel Varanovich <voronovich.pavel99@gmail.com>
2022-12-05 20:40:12 +0000

    Update 4 files

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
-const create = async () => {
-    // Write your code here 
-};
+import {writeFile} from 'fs/promises'
+import {fileURLToPath} from 'url';
+import {dirname, join} from 'path';
 
-await create();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const create = async () => {
+    const fileInnerText = 'I am fresh and young';
+    const errorMassage = 'FS operation failed';
+    const freshPath = join(__dirname, 'files', 'fresh.txt');
+
+    try {
+        await writeFile(freshPath, fileInnerText, {flag: 'wx'})
+    } catch (e) {
+        console.log(errorMassage)
+    }
+};
+create();
